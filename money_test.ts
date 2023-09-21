81e254e7560b529f201a828938fd126e8cd8ffb8
tadashi-aikawa <syou.maman@gmail.com>
2023-09-21 23:30:16 +0900

    [2章] テストコードの書き直し

--- money_test.ts
+++ money_test.ts
@@ -4,9 +4,9 @@ import { Dollar } from "./Dollar.ts";
 Deno.test("Moneyのテスト", async (t) => {
   await t.step("multiplicationのテスト", () => {
     const five = new Dollar(5);
-    five.times(2);
-    assertEquals(five.amount, 10);
-    five.times(3);
-    assertEquals(five.amount, 15);
+    let product = five.times(2);
+    assertEquals(product.amount, 10);
+    product = five.times(3);
+    assertEquals(product.amount, 15);
   });
 });
