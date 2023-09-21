e3be991c8d1cb81cebbcb24b9a7a4e8851b308b2
tadashi-aikawa <syou.maman@gmail.com>
2023-09-21 23:58:21 +0900

    [6章] DollarクラスのequalsをMoneyクラスに移動

--- Money.ts
+++ Money.ts
@@ -1,4 +1,9 @@
 export class Money {
   constructor(protected amount: number) {
   }
+
+  equals(object: object): boolean {
+    const money = object as Money;
+    return this.amount === money.amount;
+  }
 }
