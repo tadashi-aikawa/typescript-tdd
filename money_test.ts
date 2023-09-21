b6351e3d98c78b3f5e1f5d60e223b14045413fc7
tadashi-aikawa <syou.maman@gmail.com>
2023-09-21 23:46:04 +0900

    [4章] フォーマット調整

--- money_test.ts
+++ money_test.ts
@@ -4,8 +4,10 @@ import { Dollar } from "./Dollar.ts";
 Deno.test("Moneyのテスト", async (t) => {
   await t.step("multiplicationのテスト", () => {
     const five = new Dollar(5);
+
     let product = five.times(2);
     assertEquals(product.amount, 10);
+
     product = five.times(3);
     assertEquals(product.amount, 15);
   });
