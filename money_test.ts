ed16d000a8a04976616bbd43bdac175632291ebc
tadashi-aikawa <syou.maman@gmail.com>
2023-09-21 23:50:52 +0900

    [5章] フランの通らないテストコードを仮作成

--- money_test.ts
+++ money_test.ts
@@ -8,6 +8,12 @@ Deno.test("Moneyのテスト", async (t) => {
     assertEquals(five.times(3), new Dollar(15));
   });
 
+  await t.step("フランのmultiplicationのテスト", () => {
+    const five = new Franc(5);
+    assertEquals(five.times(2), new Franc(10));
+    assertEquals(five.times(3), new Franc(15));
+  });
+
   await t.step("equalityのテスト", () => {
     assert(new Dollar(5).equals(new Dollar(5)));
     assertFalse(new Dollar(5).equals(new Dollar(6)));
