5e4e58c6ed8457ec1d95705413d9baa9d85fa39c
tadashi-aikawa <syou.maman@gmail.com>
2023-09-21 23:27:44 +0900

    [2章] 副作用で失敗するテストを追加

--- money_test.ts
+++ money_test.ts
@@ -6,5 +6,7 @@ Deno.test("Moneyのテスト", async (t) => {
     const five = new Dollar(5);
     five.times(2);
     assertEquals(five.amount, 10);
+    five.times(3);
+    assertEquals(five.amount, 15);
   });
 });
