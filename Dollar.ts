8c05fdc7db2376861d18296fda8660ae5721c058
tadashi-aikawa <syou.maman@gmail.com>
2023-09-23 00:36:19 +0900

    [9章] コンストラクタをMoneyに引き上げ

--- Dollar.ts
+++ Dollar.ts
@@ -2,9 +2,6 @@ import { Money } from "./Money.ts";
 import * as factory from "./money-factory.ts";
 
 export class Dollar extends Money {
-  constructor(amount: number, currency: string) {
-    super(amount, currency);
-  }
   times(multiplier: number): Money {
     return factory.dollar(this.amount * multiplier);
   }
--- Franc.ts
+++ Franc.ts
@@ -2,9 +2,6 @@ import { Money } from "./Money.ts";
 import * as factory from "./money-factory.ts";
 
 export class Franc extends Money {
-  constructor(amount: number, currency: string) {
-    super(amount, currency);
-  }
   times(multiplier: number): Money {
     return factory.franc(this.amount * multiplier);
   }
