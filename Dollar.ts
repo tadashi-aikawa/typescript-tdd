4056acee7967701e2ec315c1f05051e01483f907
tadashi-aikawa <syou.maman@gmail.com>
2023-09-22 22:51:14 +0900

    [8章] equlsメソッドのシグニチャをDollarとFrancで一致させる

--- Dollar.ts
+++ Dollar.ts
@@ -1,7 +1,7 @@
 import { Money } from "./Money.ts";
 
 export class Dollar extends Money {
-  times(multiplier: number): Dollar {
+  times(multiplier: number): Money {
     return new Dollar(this.amount * multiplier);
   }
 }
--- Franc.ts
+++ Franc.ts
@@ -1,7 +1,7 @@
 import { Money } from "./Money.ts";
 
 export class Franc extends Money {
-  times(multiplier: number): Franc {
+  times(multiplier: number): Money {
     return new Franc(this.amount * multiplier);
   }
 }
