d66237093db29c8bd5acaf13c149318061ec49a2
tadashi-aikawa <syou.maman@gmail.com>
2023-09-22 23:04:31 +0900

    [8章] Francクラスも同様に

--- Money.test.ts
+++ Money.test.ts
@@ -1,26 +1,24 @@
 import { assert, assertEquals, assertFalse } from "assert";
-import { Dollar } from "./Dollar.ts";
-import { Franc } from "./Franc.ts";
 import { Money } from "./Money.ts";
 
 Deno.test("Moneyのテスト", async (t) => {
   await t.step("multiplicationのテスト", () => {
     const five: Money = Money.dollar(5);
-    assertEquals(five.times(2), new Dollar(10));
-    assertEquals(five.times(3), new Dollar(15));
+    assertEquals(five.times(2), Money.dollar(10));
+    assertEquals(five.times(3), Money.dollar(15));
   });
 
   await t.step("フランのmultiplicationのテスト", () => {
-    const five = new Franc(5);
-    assertEquals(five.times(2), new Franc(10));
-    assertEquals(five.times(3), new Franc(15));
+    const five = Money.franc(5);
+    assertEquals(five.times(2), Money.franc(10));
+    assertEquals(five.times(3), Money.franc(15));
   });
 
   await t.step("equalityのテスト", () => {
-    assert(new Dollar(5).equals(new Dollar(5)));
-    assertFalse(new Dollar(5).equals(new Dollar(6)));
-    assert(new Franc(5).equals(new Franc(5)));
-    assertFalse(new Franc(5).equals(new Franc(6)));
-    assertFalse(new Franc(5).equals(new Dollar(5)));
+    assert(Money.dollar(5).equals(Money.dollar(5)));
+    assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+    assert(Money.franc(5).equals(Money.franc(5)));
+    assertFalse(Money.franc(5).equals(Money.franc(6)));
+    assertFalse(Money.franc(5).equals(Money.dollar(5)));
   });
 });
--- Money.ts
+++ Money.ts
@@ -1,8 +1,8 @@
 import { Dollar } from "./Dollar.ts";
+import { Franc } from "./Franc.ts";
 
 export abstract class Money {
-  constructor(protected amount: number) {
-  }
+  constructor(protected amount: number) {}
 
   abstract times(multiplier: number): Money;
 
@@ -10,10 +10,16 @@ export abstract class Money {
     return new Dollar(amount);
   }
 
+  static franc(amount: number): Money {
+    return new Franc(amount);
+  }
+
   equals(object: object): boolean {
     const money = object as Money;
 
-    return this.amount === money.amount &&
-      this.constructor.name === object.constructor.name;
+    return (
+      this.amount === money.amount &&
+      this.constructor.name === object.constructor.name
+    );
   }
 }
