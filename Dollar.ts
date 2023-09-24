c742823d69cd1377568ff1975787e2aaf8999343
tadashi-aikawa <syou.maman@gmail.com>
2023-09-24 22:47:55 +0900

    [11章] Dollarクラスの削除

--- Dollar.ts
+++
@@ -1,7 +0,0 @@
-import { Money } from "./Money.ts";
-
-export class Dollar extends Money {
-  constructor(amount: number, currency: string) {
-    super(amount, currency);
-  }
-}
--- money-factory.ts
+++ money-factory.ts
@@ -1,11 +1,9 @@
-import { Dollar } from "./Dollar.ts";
-import { Franc } from "./Franc.ts";
 import { Money } from "./Money.ts";
 
 export function dollar(amount: number): Money {
-  return new Dollar(amount, "USD");
+  return new Money(amount, "USD");
 }
 
 export function franc(amount: number): Money {
-  return new Franc(amount, "CHF");
+  return new Money(amount, "CHF");
 }
