5a820af17c3c0ab6764dd7414f97d2ccb43bc89e
tadashi-aikawa <syou.maman@gmail.com>
2023-09-27 17:23:12 +0900

    [13章] reduceの仮実装2

--- Bank.ts
+++ Bank.ts
@@ -1,9 +1,12 @@
 import * as factory from "./money-factory.ts";
 import { Expression } from "./Expression";
 import { Money } from "./Money";
+import { Sum } from "./Sum.ts";
 
 export class Bank {
   reduce(source: Expression, to: string): Money {
-    return factory.dollar(10);
+    const sum = source as Sum;
+    const amount = sum.augend.amount + sum.addend.amount;
+    return new Money(amount, to);
   }
 }
