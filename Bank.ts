f611392478377ca12f580bef74e9ee2e3284747e
tadashi-aikawa <syou.maman@gmail.com>
2023-09-27 18:12:44 +0900

    [14章] 為替レートの設定処理を実装

--- Bank.ts
+++ Bank.ts
@@ -1,12 +1,16 @@
 import { Expression } from "./Expression";
 import { Money } from "./Money";
+import { Pair } from "./Pair";
 
 export class Bank {
+  private rates: Map<Pair, number> = new Map();
   reduce(source: Expression, to: string): Money {
     return source.reduce(this, to);
   }
 
-  addRate(to: string, from: string, rate: number) {}
+  addRate(to: string, from: string, rate: number) {
+    this.rates.set(new Pair(from, to), rate);
+  }
 
   rate(from: string, to: string): number {
     return from === "CHF" && to === "USD" ? 2 : 1;
