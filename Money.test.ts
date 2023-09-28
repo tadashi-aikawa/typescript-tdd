b910f410a0d7f714a9666c41eeb08b58273da973
tadashi-aikawa <syou.maman@gmail.com>
2023-09-28 16:29:55 +0900

    [15章] mixedAdditionのテストでplusの引数にあたる値の型をMoneyからExpressionに変更

--- Money.test.ts
+++ Money.test.ts
@@ -12,7 +12,7 @@ describe("Moneyのテスト", () => {
 
   test("mixedAdditionのテスト", () => {
     const fiveBucks: Money = factory.dollar(5);
-    const tenFranc: Money = factory.franc(10);
+    const tenFranc: Expression = factory.franc(10);
     const bank = new Bank();
     bank.addRate("CHF", "USD", 2);
     const result = bank.reduce(fiveBucks.plus(tenFranc), "USD");
