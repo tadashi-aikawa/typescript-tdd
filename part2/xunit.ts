4562f0ccd7778ea424e104982c2be532a3effbc5
tadashi-aikawa <syou.maman@gmail.com>
2023-10-01 16:19:19 +0900

    [18章] xunit.ts以外にファイルを作らない約束だったので集約

--- part2/xunit.ts
+++ part2/xunit.ts
@@ -1,4 +1,20 @@
-import { WasRun } from "./WasRun";
+export class TestCase {
+  constructor(public name: string) {}
+
+  run() {
+    ((this as any)[this.name] as CallableFunction)();
+  }
+}
+
+export class WasRun extends TestCase {
+  constructor(name: string, public wasRun?: number) {
+    super(name);
+  }
+
+  testMethod() {
+    this.wasRun = 1;
+  }
+}
 
 const test = new WasRun("testMethod");
 console.log(test.wasRun);
